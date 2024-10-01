57faef27ac6b4d41dbdc18556027927ffbe53630
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-09-30 23:32:11 -0300

    chore: initial project structure

---
+++ src/main/java/me/diegopagotto/piwhois/PiWhoIsApplication.java
@@ -0,0 +1,13 @@
+package me.diegopagotto.piwhois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiWhoIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiWhoIsApplication.class, args);
+	}
+
+}
